273b11d59dedacc27b11b3eb1c6f1f86d00815dd
jaensen <daniel@djanz.de>
2021-04-17 22:03:22 +0200

    added a key cache to the auth client to prevent unnecessary requests to the auth service

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -9,6 +9,48 @@ export class Client
   private readonly _issuer: string;
   private readonly _appId: string;
 
+  private static _cacheLookupCounter = 0;
+  private static readonly _maxCacheAgeInMs = 1000 * 60 * 60 * 24;
+  private static readonly _cacheCleanupAfterXLookups = 1000;
+
+  private static readonly _keyCache: {
+    [kid:string]: {
+      lastUsed: Date,
+      key: any
+    }
+  } = {};
+
+  private static async getKey(kid:string) : Promise<any>
+  {
+    Client._cacheLookupCounter++;
+
+    if (Client._keyCache[kid]) {
+      Client._keyCache[kid].lastUsed = new Date();
+      return Client._keyCache[kid].key;
+    }
+
+    const downloadedKey = await fetch(kid).then((data: any) => data.json());
+
+    if (Client._cacheLookupCounter % Client._cacheCleanupAfterXLookups == 0)
+    {
+      Client._cacheLookupCounter = 0;
+
+      const minLastUsed = Date.now() - Client._maxCacheAgeInMs;
+      Object.keys(Client._keyCache).forEach(kid => {
+        if (Client._keyCache[kid].lastUsed.getTime() < minLastUsed) {
+          delete Client._keyCache[kid];
+        }
+      });
+    }
+
+    Client._keyCache[kid] = {
+      lastUsed: new Date(),
+      key: downloadedKey
+    };
+
+    return downloadedKey;
+  }
+
   constructor(appId: string, issuer: string)
   {
     this._issuer = issuer;
@@ -31,7 +73,7 @@ export class Client
     if (!kid)
       throw new Error("No key id (kid) claim.")
 
-    const key = await fetch(kid).then((data: any) => data.json());
+    const key = await Client.getKey(kid);
     const pubKey = key.data.keys.publicKey;
     if (!pubKey)
       throw new Error("Couldn't fetch the public key to verify the jwt");
