4c794608561d3c93f847be9698bb971b3d8bf44f
jaensen <daniel@djanz.de>
2021-04-17 15:47:51 +0200

    added a environment variable for the ssl cipher and the smtp-sender

--- packages/mailer/src/mailer.ts
+++ packages/mailer/src/mailer.ts
@@ -7,24 +7,36 @@ export class Mailer
     static async send(template:Template, data:object, to:string)
     {
         if (!process.env.SMTP_USER
-        || !process.env.SMTP_PORT
-        || !process.env.SMTP_SECURE
-        || !process.env.SMTP_USER
-        || !process.env.SMTP_PASS)
+         || !process.env.SMTP_PORT
+         || !process.env.SMTP_USER
+         || !process.env.SMTP_PASS
+         || !process.env.SMTP_SENDER)
             throw new Error("One of the SMTP settings is missing in the environment variables");
+
+        const tls: {
+            ciphers?: string;
+        } = {};
+
+        if (process.env.SMTP_SECURE) {
+            if (!process.env.SMTP_SECURE_CIPHERS) {
+                throw new Error(`The process.env.SMTP_SECURE_CIPHERS environment variable is not set`);
+            }
+            tls.ciphers = process.env.SMTP_SECURE_CIPHERS;
+        }
 
         const transport = nodemailer.createTransport({
             host: process.env.SMTP_SERVER,
             port: parseInt(process.env.SMTP_PORT),
-            secure: process.env.SMTP_SECURE.toLowerCase() == "true",
+            secure: process.env.SMTP_SECURE?.toLowerCase() == "true" ?? false,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS
-            }
+            },
+            tls: tls
         });
 
         await transport.sendMail({
-            from: "mamaomo@omo.earth",
+            from: process.env.SMTP_SENDER,
             to: to,
             subject: mustache.render(template.subject, data),
             html: mustache.render(template.bodyHtml, data),
